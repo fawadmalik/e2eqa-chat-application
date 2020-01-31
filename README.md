6226342fb8320abacab996c4a9acf89e99d73460
Fawad Malik <fawad.malik@hotmail.com>
2020-01-30 23:18:07 -0800

    Adds starter code for the project. Needs fixing errors

---
+++ README.md
@@ -0,0 +1,1 @@
+"# e2eqa-chat-application" 
---
+++ src/main/java/com/e2eqa/chatapp/App.java
@@ -0,0 +1,13 @@
+package com.e2eqa.chatapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
